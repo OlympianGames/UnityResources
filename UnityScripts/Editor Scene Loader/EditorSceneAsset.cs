9d7c1a885591ff2dcd0b4482fb39d590cf12a4f4
copper <79734708+OlympianGames@users.noreply.github.com>
2022-12-03 20:53:57 -0600

    Add files via upload

---
+++ UnityScripts/Editor Scene Loader/EditorSceneAsset.cs
@@ -0,0 +1,20 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Editor Scene Asset", menuName = "Editor Scene Asset")]
+public class EditorSceneAsset : ScriptableObject 
+{
+    public List<SceneListItem> scenes;
+}
+
+[System.Serializable]
+public class SceneListItem
+{
+    public string sceneName;
+    public SceneAsset sceneAsset;
+}
+
+#endif
