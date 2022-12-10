82538404e754bfa013eaf3979c7f5138a41b1cae
copper <79734708+OlympianGames@users.noreply.github.com>
2022-12-09 23:44:47 -0600

    Update EditorSceneAsset.cs

--- UnityScripts/Editor Scene Loader/EditorSceneAsset.cs
+++ UnityScripts/Editor Scene Loader/EditorSceneAsset.cs
@@ -6,8 +6,9 @@
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
-[CreateAssetMenu(fileName = "Editor Scene Asset", menuName = "Editor Scene Asset")]
+[CreateAssetMenu(fileName = "Editor Scene Object", menuName = "Editor Scene Object")]
 public class EditorSceneAsset : ScriptableObject 
 {
     public List<SceneListItem> scenes;
@@ -18,6 +19,83 @@ public class SceneListItem
 {
     public string sceneName;
     public SceneAsset sceneAsset;
+}
+
+[CustomEditor(typeof(EditorSceneAsset))]
+public class EditorSceneAssetInspector : Editor 
+{
+    EditorSceneAsset script;
+    private void OnEnable() 
+    {
+        script = target as EditorSceneAsset;
+    }
+
+    public override void OnInspectorGUI() 
+    {
+        if(script == null)
+            script = target as EditorSceneAsset;
+
+        base.OnInspectorGUI();
+        
+        EditorGUILayout.Space(10);
+
+        try
+        {
+            if(CurrentSceneInList())
+            {
+                // EditorGUILayout.LabelField("Scene Not In List");
+                InspectorHelper.Button("Remove Current Scene", RemoveCurrentSceneFromList);
+            }
+            else
+            {
+                // EditorGUILayout.LabelField("Scene In List");
+                InspectorHelper.Button("Add Current Scene", AddCurrentSceneToList);
+            } 
+        }
+        catch
+        {
+            // error
+        }
+    }
+
+    private bool CurrentSceneInList()
+    {
+        if(script.scenes.Count > 0)
+        {
+            foreach (var item in script.scenes)
+            {
+                if(SceneManager.GetActiveScene().path == AssetDatabase.GetAssetPath(item.sceneAsset))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void AddCurrentSceneToList()
+    {
+        SceneListItem listItem = new SceneListItem();
+
+
+        listItem.sceneAsset = (SceneAsset)AssetDatabase.LoadAssetAtPath(SceneManager.GetActiveScene().path, typeof(SceneAsset));
+        listItem.sceneName = listItem.sceneAsset.name;
+
+        script.scenes.Add(listItem);
+    }
+
+    private void RemoveCurrentSceneFromList()
+    {
+        foreach (var item in script.scenes)
+        {
+            if(SceneManager.GetActiveScene().path == AssetDatabase.GetAssetPath(item.sceneAsset))
+            {
+                script.scenes.Remove(item);
+                return;
+            }
+        }
+    }
 }
 
 #endif
